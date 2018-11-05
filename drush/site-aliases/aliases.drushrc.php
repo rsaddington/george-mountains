bd42a23e869ec034bc5bb1ed1b1ca5ad5f4d3863
Rich Saddington <rich.saddington@Richs-MacBook-Pro.local>
2018-11-05 13:52:26 +0000

    BLT-122: up and running locally.

--- drush/site-aliases/aliases.drushrc.php
+++ drush/site-aliases/aliases.drushrc.php
@@ -374,3 +374,18 @@ if ('vagrant' != $_SERVER['USER']) {
   );
 }
 
+
+// Local environment.
+$aliases['george-mountains.local'] = array(
+  'root' => '/var/www/george-mountains/docroot',
+  'uri' => 'http://local.george-mountains.com',
+  );
+// Add remote connection options when alias is used outside VM.
+if ('vagrant' != $_SERVER['USER']) {
+  $aliases['george-mountains.local'] += array(
+    'remote-host' => 'local.george-mountains.com',
+    'remote-user' => 'vagrant',
+    'ssh-options' => '-o PasswordAuthentication=no -i ' . drush_server_home() . '/.vagrant.d/insecure_private_key'
+  );
+}
+
